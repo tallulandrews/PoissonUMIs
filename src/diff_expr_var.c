fb0b63e0e7c145c23149fdc9b6586f5ad00430e0
Tallulah Andrews <ta6@sanger.ac.uk>
2016-10-14 11:33:13 +0100

    Start Modifying for Poisson-lognormal

--- src/diff_expr_var.c
+++ src/diff_expr_var.c
@@ -18,19 +18,23 @@ this program . If not , see <http://www.gnu.org/licenses/>. */
 #include <stdio.h>
 #include <stdlib.h>
 #include "misc_functions.h"
+#include "poilog_package_C_code.h"
 
 double calc_gene_log_likelihood (double* val_mat, double* lambda_mat, int* ncol, int* nrow, int gene_row) {
 	int col;
 	double likelihood = 1.0;
 	for (col = 0; col < *ncol; col++) {
 		int coord = convert_2D_indices_to_1D(gene_row, col, nrow, ncol);
-		likelihood = likelihood + log(dPoisson( (int)val_mat[coord], lambda_mat[coord] ));
+		double rounded_val = ceil(val_mat[coord]);
+		double prob = dPoisson( (int)rounded_val, lambda_mat[coord] );
+		if (prob < 0.0001) {prob = 0.0001;}
+		likelihood = likelihood + log(prob);
 	}
 	return(likelihood);
 }
 
 double chi_sq_test (double chi_sq, int df) {
-	return( pPoisson(df/2, chi_sq/2.0) );
+	return( pPoisson(df/2, chi_sq/2.0) ); //Is integer division a problem here?
 }
 
 /* output should be a nrow x nlabels+1 matrix*/
@@ -38,33 +42,66 @@ double chi_sq_test (double chi_sq, int df) {
 void diff_expression (double* y, double* lambdas, int* nrow, int* ncol, int* labels, int* nlabels, double* out) {
 	int row1, column; 
 //	double group_lambdas[*nrow * *ncol];
+
+	/* Calculate lambdas where each group has it's own mean expression for 
+	the gene. 
+	This is calculated by scaling the existing global lambda by a factor
+	which represents the mean expression in the group relative to the
+	global mean expression.*/
 	double* group_lambdas = (double*)malloc(*nrow * *ncol * sizeof(double));
 	int* ncol_out = (int*)malloc(sizeof(int));
 	*ncol_out = *nlabels+1;
 	for (row1 = 0; row1 < *nrow; row1++) {
-		long group_sums[*nlabels];
-		int group_size[*nlabels];
-		long row_sum;
-		for (column = 0; column < *ncol; column++) {
-			double val = y[convert_2D_indices_to_1D(row1, column, nrow, ncol)];
+
+		/* Calculate expression & size of each group */
+		long group_sums[*nlabels]; //total molecules per group
+		long group_lambda_sums[*nlabels]; //total molecules per group
+		int group_size[*nlabels]; //number of cells per group
+		long row_sum = 0; //total molecules for this gene across all cells
+		int row_size = 0;
+
+		// initialize
+		int tmp;
+		for (tmp = 0; tmp < *nlabels; tmp++) {
+			group_size[tmp] = 0;
+			group_sums[tmp] = 0;
+			group_lambda_sums[tmp] = 0;
+		}
+
+		for (column = 0; column < *ncol; column++) { //across cells
+			int index = convert_2D_indices_to_1D(row1, column, nrow, ncol);
+			double val = y[index];
+			val = ceil(val);
 			row_sum = row_sum + val;
+			row_size = row_size + 1;
 			int group_id = labels[column];
 			group_sums[group_id] = group_sums[group_id] + val;
+			group_lambda_sums[group_id] = group_lambda_sums[group_id] + lambdas[index];
 			group_size[group_id] = group_size[group_id] + 1;
 		}
-		for (column = 0; column < *ncol; column++) {
+		/* Scale each lambda by the relevant group factor */
+		for (column = 0; column < *ncol; column++) { 
 			int index = convert_2D_indices_to_1D(row1, column, nrow, ncol);
 			int group_id = labels[column];
-			double group_factor = group_size[group_id] * (double)row_sum / ((double) *nrow);
-			group_lambdas[index] = lambdas[index] * ( (double)group_sums[group_id] / group_factor );
-			out[convert_2D_indices_to_1D(row1, group_id, nrow, ncol_out)] = (double) group_sums[group_id]/group_size[group_id]; //store mean expression for each group in output
+			double group_factor = 1.0;
+			if (group_sums[group_id] > 0) {
+				group_factor = ((double)group_sums[group_id] / group_size[group_id] ) / ( (double)row_sum / (row_size)); //relative mean expression
+			} else {
+				if (group_size[group_id] > 0) {
+					group_factor = 1.0/ (double)group_size[group_id];
+				}
+			}
+
+			group_lambdas[index] = lambdas[index] * group_factor;
+			out[convert_2D_indices_to_1D(row1, group_id, nrow, ncol_out)] = ( (double)group_sums[group_id] - (double) group_lambda_sums[group_id] )/group_size[group_id]; //store mean expression for each group in output
 		}
 		double likelihood_null = calc_gene_log_likelihood(y, lambdas, ncol, nrow, row1);
-		double likelihood_alt = calc_gene_log_likelihood(y, group_lambdas, ncol, nrow, row1); //This may not work b/c group_lambdas might be restricted in scope to this fxn - now malloced so should be fine.
+		double likelihood_alt = calc_gene_log_likelihood(y, group_lambdas, ncol, nrow, row1);
 		double log_ratio = likelihood_alt-likelihood_null;
-		double stat = -2.0*log_ratio;
-		double pval = chi_sq_test(stat, *nlabels-1);
-		out[convert_2D_indices_to_1D(row1, *nlabels+1, nrow, ncol_out)] = pval;
+		if (log_ratio < 0) {log_ratio=0;}
+		double stat = 2.0*log_ratio;
+		double pval = chi_sq_test(stat, (*nlabels)-1);
+		out[convert_2D_indices_to_1D(row1, (*nlabels), nrow, ncol_out)] = pval;
 	}
 }
 
